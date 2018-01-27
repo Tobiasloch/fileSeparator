a009653f97efd4ef9e64b23eb1f3a819e8219cd9
Tobias Loch <Tobias.Loch@online.de>
2018-01-27 10:52:21 +0100

    renaimed main to mainclass

--- FileSeparator/src/mainClass.java
+++ FileSeparator/src/mainClass.java
@@ -1,7 +1,7 @@
 import javax.swing.UIManager;
 
-public class main {
-
+public class mainClass {
+	
 	public static void main(String[] args) {
 		try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
